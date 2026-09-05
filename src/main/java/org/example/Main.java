a81b8059ff93b16b390594fb1a115bee4ae130a1
DimaNoyt <qwaszxcvfdertgb@gmail.com>
2026-09-05 11:47:00 +0300

    Rook moves, LongRangePiece

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,7 +10,7 @@ public class Main {
 //        board.setupDefaultPiecesPositions();
 
         Board board = (new BoardFactory()).fromFEN(
-                "3k4/8/5n2/2N5/3B4/8/8/3K4 w - - 0 1"
+                "5n2/2p5/3B4/8/k2R1p2/8/8/3K4 w KQkq - 0 1"
         );
 
         BoardConsoleRenderer renderer = new BoardConsoleRenderer();
--- src/main/java/org/example/piece/Rook.java
+++ src/main/java/org/example/piece/Rook.java
@@ -1,17 +1,34 @@
 package org.example.piece;
 
+import org.example.Board;
+import org.example.BoardUtils;
 import org.example.Color;
 import org.example.Coordinates;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
-public class Rook extends Piece {
+public class Rook extends LongRangePiece {
     public Rook(Color color, Coordinates coordinates) {
         super(color, coordinates);
     }
 
     @Override
     protected Set<CoordinatesShift> getPieceMoves() {
-        return Set.of();
+
+        Set<CoordinatesShift> result = new HashSet<>();
+
+        for (int i = -7; i <= 7; i++) {
+            if (i == 0) continue;
+
+            result.add(new CoordinatesShift(i,0));
+        }
+
+        for (int i = -7; i <= 7; i++) {
+            if (i == 0) continue;
+            result.add(new CoordinatesShift(0,i));
+        }
+        return result;
     }
 }
