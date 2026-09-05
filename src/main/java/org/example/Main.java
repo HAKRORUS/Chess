2d858df5030487f515c89c51892a6f024e6bbe52
DimaNoyt <qwaszxcvfdertgb@gmail.com>
2026-09-05 20:43:28 +0300

    Rook moves, LongRangePiece

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,7 +15,7 @@ public class Main {
 
         BoardConsoleRenderer renderer = new BoardConsoleRenderer();
 
-        //test
+        //testgit config --global http.sslVerify false
 
         Game game = new Game(board);
         game.gameLoop();
